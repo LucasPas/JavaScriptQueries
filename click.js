4d7143a25a863f874fec7932060c10a11ec29dae
LucasPas <7lpasiewicz@stpatrick.org>
2017-03-23 10:47:33 -0500

    Finished Click, moved to resize

--- click.js
+++ click.js
@@ -1,18 +1,22 @@
 //declare samecolor() here
-function samecolor(hex){
+function samecolor(){
   const doc= document;
   let divs = doc.querySelectorAll("#samecolor div");
   for(let i=0; i<divs.length;i++){
-    divs[i].style.backgroundColor = "#"+hex;
+    divs[i].style.backgroundColor = "#123456";
     divs[i].style.opacity= Math.random();
-    console.log("jhsdfklj");
   }
-console.log("asdf");
 }
 //declare diffcolor() here
 function diffcolor(hex){
   const doc2= document;
-  let divs2= doc.querySelectorAll("#diffcolor div");
-  
-
+  let divs2= doc2.querySelectorAll("#diffcolor div");
+  for(let i=0; i<divs2.length;i++){
+    if(i==0 || i==2 || i==4 || i==6 || i==8){
+      divs2[i].style.backgroundColor = "#008732";
+    }
+    else{
+      divs2[i].style.backgroundColor = "#FF78CD";
+    }
+  }
 }
--- resize.js
+++ resize.js
@@ -1,4 +1,10 @@
 //Declare tabletSize() here
-
+function tabletSize(){
+  const doc= document;
+  let size = doc.querySelectorAll("section div");
+  let height = "800px";
+  let width = "1440px";
+  
+}
 
 //Declare phoneSize() here
